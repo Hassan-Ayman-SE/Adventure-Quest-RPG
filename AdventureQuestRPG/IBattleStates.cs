d2e22d10c3b7391a41eea9efeb8b673ef53e64a2
Hassan Karraz <24042732@student.ltuc.com>
2024-07-06 15:53:35 +0300

    ADD IBattleStates Interface

---
+++ AdventureQuestRPG/IBattleStates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuestRPG
+{
+    internal interface IBattleStates
+    {
+        //props
+        string Name { get; set; }
+        int Health { get; set; }
+        int AttackPower { get; set; }
+        int Defense { get; set; }
+    }
+}
